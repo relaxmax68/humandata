9811a293c8fb5ff386e819eaec6e3a9b65ef073f
relaxmax@spire <maxime.code@bbox.fr>
2015-12-23 21:30:44 +0100

    page accueil opérationnelle avec liens actifs

--- src/AccueilBundle/Controller/DefaultController.php
+++ src/AccueilBundle/Controller/DefaultController.php
@@ -10,4 +10,28 @@ class DefaultController extends Controller
     {
         return $this->render('AccueilBundle:Default:index.html.twig');
     }
+    public function accueilAction()
+    {
+        return $this->render('AccueilBundle:Default:index.html.twig');
+    }
+    public function gestionDeRappelsAction()
+    {
+        return $this->render('AccueilBundle:Default:gestionDeRappels.html.twig');
+    }
+    public function clubRHAction()
+    {
+        return $this->render('AccueilBundle:Default:clubRH.html.twig');
+    }
+    public function miniSocialAction()
+    {
+        return $this->render('AccueilBundle:Default:miniSocial.html.twig');
+    }
+    public function SELAction()
+    {
+        return $this->render('AccueilBundle:Default:SEL.html.twig');
+    }
+    public function guideResponsableAction()
+    {
+        return $this->render('AccueilBundle:Default:guideResponsable.html.twig');
+    }
 }
