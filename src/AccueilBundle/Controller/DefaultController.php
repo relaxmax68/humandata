db22310197d4c6fa00d1d134b433d3c3f2411b1b
relaxmax@spire <maxime.code@bbox.fr>
2016-01-13 22:10:42 +0100

    +menu

--- src/AccueilBundle/Controller/DefaultController.php
+++ src/AccueilBundle/Controller/DefaultController.php
@@ -3,20 +3,37 @@
 namespace AccueilBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('AccueilBundle:Default:index.html.twig');
+        return $this->render('AccueilBundle:Default:index.html.twig',array('listProjets'=>array()));
     }
+
+    public function menuAction($limit)
+    {
+        
+        $listProjets = array(
+          array('id' => 1, 'title' => 'gestion de rappels'),
+          array('id' => 2, 'title' => 'club RH'),
+          array('id' => 3, 'title' => 'mini réseau social'),
+          array('id' => 4, 'title' => 'gestion de SEL'),
+          array('id' => 5, 'title' => 'guide de produits responsables'),          
+        );
+
+        return $this->render('AccueilBundle:Default:menu.html.twig', array(
+          'listProjets' => $listProjets
+        ));
+  }
     public function accueilAction()
     {
         return $this->render('AccueilBundle:Default:index.html.twig');
     }
-    public function gestionDeRappelsAction()
+    public function projetsAction($id)
     {
-        return $this->render('AccueilBundle:Default:gestionDeRappels.html.twig');
+        return new Response("affichage du projet ".$id);
     }
     public function clubRHAction()
     {
