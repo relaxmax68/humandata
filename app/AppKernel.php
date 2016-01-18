beace259370f0c9fcf18baeaf8f6babbc7aadeae
relaxmax@spire <maxime.code@bbox.fr>
2016-01-18 21:30:18 +0100

    +ForUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new AccueilBundle\AccueilBundle(),
             new UserBundle\UserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
