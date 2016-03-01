0dd3c1f1225a97cd95e4fef53cb68278a2e22c50
relaxmax@spire <maxime.code@bbox.fr>
2016-03-01 08:49:50 +0100

    +view /tests/header
    Conflicts:
    	src/TestsBundle/Controller/DefaultController.php
    	src/TestsBundle/Resources/config/routing.yml

--- src/TestsBundle/Controller/DefaultController.php
+++ src/TestsBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace TestsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+
 class DefaultController extends Controller
 {
     public function indexAction()
@@ -17,5 +19,20 @@ class DefaultController extends Controller
     public function structureAction()
     {
         return $this->render('TestsBundle:Tests:structure.html.twig');
+    }
+    // affiche tous les cookies enregistrés par le site
+    public function cookiesAction()
+    {
+        return $this->render('TestsBundle:Tests:cookies.html.twig');
+    }
+    // affiche le header envoyé par le site
+    public function headerAction()
+    {
+        $request = Request::createFromGlobals();
+
+        // the URI being requested (e.g. /about) minus any query parameters
+        $content=$request->getPathInfo();
+
+        return $this->render('TestsBundle:Tests:header.html.twig',array('request'=>$request,'content'=>$content));
     }
 }
